25cc2874c42c9f46bfb6a4386a8017137ab069d6
Mitko Rosenov <mitkorosenov@live.com>
2024-05-15 22:17:54 +0300

    customer and invoice controllers.

---
+++ app/Http/Controllers/v1/CustomerController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Filters\v1\CustomerFilter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\StoreCustomerRequest;
+use App\Http\Requests\v1\UpdateCustomerRequest;
+use App\Http\Resources\v1\CustomerCollection;
+use App\Http\Resources\v1\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $filter = new CustomerFilter();
+        $filterItems = $filter->transform($request);
+
+        $includeInvoices = $request->query('includeInvoices');
+
+        $customers = Customer::where($filterItems);
+
+        if ($includeInvoices) {
+            $customers = $customers->with('invoices');
+        }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        return new CustomerResource(Customer::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
