c87ab579aa5addc4664ecaacb357c0913b9fc761
Mitko Rosenov <mitkorosenov@live.com>
2024-05-29 15:28:37 +0300

    Implementation of Payment (not done yet).

---
+++ database/migrations/2024_05_28_215900_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->unsignedInteger('invoice_id')->index();
+            $table->string('ref_id');
+            $table->integer('amount');
+            $table->dateTime('payment_date');
+            $table->unsignedInteger('payment_method')->index();
+            $table->timestamps();
+
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
