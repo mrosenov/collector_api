46562a404906126bbab394bddc3752c5b15b640a
Mitko Rosenov <mitkorosenov@live.com>
2024-05-29 16:59:05 +0300

    payment factories refactor.

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Invoice;
+use App\Models\PaymentMethod;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,13 @@ class PaymentFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
-            //
+            'invoice_id' => Invoice::factory(),
+            'ref_id' => $this->faker->uuid(),
+            'amount' => $this->faker->numberBetween(1,2000),
+            'payment_date' => $this->faker->dateTimeThisDecade(),
+            'payment_method' => PaymentMethod::factory(),
         ];
     }
 }
--- database/factories/PaymentMethodFactory.php
+++ database/factories/PaymentMethodFactory.php
@@ -17,7 +17,8 @@ class PaymentMethodFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
         ];
     }
 }
