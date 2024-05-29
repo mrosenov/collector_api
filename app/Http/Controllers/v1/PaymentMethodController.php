3866977ffb9c8c5e3fce2e02d9cf731b6330ac90
Mitko Rosenov <mitkorosenov@live.com>
2024-05-29 15:30:41 +0300

    Implementing Payment Methods.

---
+++ app/Http/Controllers/v1/PaymentMethodController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\StorePaymentMethodRequest;
+use App\Http\Requests\v1\UpdatePaymentMethodRequest;
+use App\Models\PaymentMethod;
+
+class PaymentMethodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaymentMethodRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentMethod $paymentMethod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaymentMethod $paymentMethod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaymentMethodRequest $request, PaymentMethod $paymentMethod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaymentMethod $paymentMethod)
+    {
+        //
+    }
+}
